8d04d5da5e5e56ec3b569c30339c001b6bd7836b
Tushar12152 <tusharimran470@gmail.com>
2023-10-29 07:15:13 -0600

    delete from database added

--- src/components/ManangeProduct/ManageProduct.jsx
+++ src/components/ManangeProduct/ManageProduct.jsx
@@ -1,16 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
 import ManageProductDetail from "./ManageProductDetail/ManageProductDetail";
+// import { useEffect, useState } from "react";
 
 const ManageProduct = () => {
-   const {data,isLoading,}=useQuery({
+
+   const { data,isLoading,refetch}=useQuery({
        queryKey:['products'],
        queryFn:async()=>{
            const data=await fetch("http://localhost:5002/products")
            return await data.json();
-       }
+       },
+      
    })
    
-//    console.log(data);
+//    const [datas,setDatas]=useState([])
+
+
+//    useEffect(() => {
+//     if (data) {
+//       setDatas(data);
+//     }
+//   }, [data]);
+// //    console.log(datas)
+
+//    console.log(datas);
+
+
+
+
+
+
+
 
    if(isLoading){
     return <span className="loading loading-spinner text-warning"></span>
@@ -20,12 +40,24 @@ const ManageProduct = () => {
 
     return (
         <div className="grid grid-cols-1 gap-6 p-5">
+
+            {data?.length}
             {
-                data.map(product=><ManageProductDetail
+                data?
+                
+                <div>
+                      {
+                   data?.map(product=><ManageProductDetail
                     key={product._id}
+                    // datas={datas}
+                    // setDatas={setDatas}
+                    refetch={refetch}
                     product={product}
                 ></ManageProductDetail>)
 
+            }
+                </div>:
+                <div className="text-black text-center mt-20 ">You have no any product ... please add product first.....</div>
             }
         </div>
     );
--- src/components/ManangeProduct/ManageProductDetail/ManageProductDetail.jsx
+++ src/components/ManangeProduct/ManageProductDetail/ManageProductDetail.jsx
@@ -1,10 +1,51 @@
+import axios from "axios";
 import PropTypes from "prop-types";
 import { AiFillDelete } from 'react-icons/ai';
 import { GrUpdate } from 'react-icons/gr';
+import swal from "sweetalert";
 
 
-const ManageProductDetail = ({ product }) => {
-  const { name, category, price, description, photo } = product;
+const ManageProductDetail = ({ product,datas,setDatas ,refetch}) => {
+  const {_id, name, category, price, description, photo } = product || {};
+
+
+  const handleDelete=id=>{
+
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+       
+
+            axios.delete(`http://localhost:5002/products/${id}`)
+            .then(res=>{
+            //  console.log(res.data)
+            if(res.data.deletedCount>0){
+                swal(`${name}! Your imaginary product has been deleted!`, {
+                    icon: "success", });
+                    refetch()
+                    // const remaining=datas?.filter(data=>data._id!==id)
+                    // setDatas(remaining)
+            }
+           
+            })
+
+
+        } else {
+          swal("Your imaginary product is safe!");
+        }
+      });
+
+
+
+
+       
+  }
 
   return (
     <div>
@@ -22,7 +63,7 @@ const ManageProductDetail = ({ product }) => {
           <p>Category: {category}</p>
           <p> price: {price} Taka</p>
           <div className="card-actions   justify-end">
-            <button className="btn bg-gradient-to-r from-pink-200 to-red-500"><AiFillDelete></AiFillDelete></button>
+            <button onClick={()=>handleDelete(_id)} className="btn bg-gradient-to-r from-pink-200 to-red-500"><AiFillDelete></AiFillDelete></button>
 
             <button className="btn bg-gradient-to-r from-pink-200 to-red-500"><GrUpdate></GrUpdate></button>
           </div>
@@ -34,6 +75,9 @@ const ManageProductDetail = ({ product }) => {
 
 ManageProductDetail.propTypes = {
   product: PropTypes.object.isRequired,
+  datas:PropTypes.array,
+  setDatas:PropTypes.any,
+  refetch:PropTypes.func
 };
 
 export default ManageProductDetail;
