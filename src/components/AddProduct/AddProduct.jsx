9e2f23da6ec4eb894b2212ccbe5d3aafda0a3228
Tushar12152 <tusharimran470@gmail.com>
2023-10-29 00:20:04 -0600

    add products form added

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,8 +1,73 @@
 
 const AddProduct = () => {
+
+    const AddData=e=>{
+        e.preventDefault()
+        const form=e.target;
+        const name=form.productName?.value || 'not given';
+        const category=form.category?.value  || 'not given';
+        const price=form.price?.value  || 'not given';
+        const description=form.description?.value  || 'not given';
+        const photo=form.photo?.value  || 'not given';
+
+        const  product={
+            name,category,price,description,photo
+        }
+
+        console.log(product)
+    }
+
+
     return (
         <div>
-            <h1>addd</h1>
+            <form onSubmit={AddData} className="card-body">
+
+
+            <div className="form-control">
+          <label className="label">
+           
+          </label>
+         <div className="flex gap-6 w-full">
+         <input type="text" name="productName" placeholder="Product Name" className="input input-bordered w-[50%]" required />
+         
+   
+    
+         <input type="text" name="category" placeholder="Product Category" className="input input-bordered w-[50%]" required />
+      
+         </div>
+        </div>
+
+
+
+        <div className="form-control">
+          <label className="label">
+           
+          </label>
+         <div className="flex gap-6 w-full">
+         <input type="text" name="price" placeholder="Price" className="input input-bordered w-[50%]" required />
+         
+   
+    
+         <input type="text" name="description" placeholder="Description" className="input input-bordered w-[50%]" required />
+      
+         </div>
+        </div>
+
+
+
+        <div className="form-control">
+          <label className="label">
+          
+          </label>
+          <input type="text" name="photo" placeholder="Product Photo URL" className="input input-bordered" required />
+          <label className="label">
+          
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn b bg-gradient-to-r from-pink-200 to-blue-200">Add Poduct</button>
+        </div>
+      </form>
         </div>
     );
 };
--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -9,10 +9,10 @@ const DashBoard = () => {
                  <Nav></Nav>
 
 
-              <div className="grid grid-cols-12 w-11/12 mx-auto mt-5 ">
+              <div className="grid grid-cols-12 w-11/12 gap-2 mx-auto mt-5 ">
                 <div className="bg-red-100 col-span-3 h-screen flex flex-col items-center gap-6">
 
-                <NavLink className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "bg-blue-100 p-2 my-2 rounded-xl  border border-red-900 w-full  " : "p-2 my-2 rounded-xl  border border-red-900 w-full"  } to='/dashboard'>Home</NavLink>
+                <NavLink className={({ isActive, isPending }) => isPending ? "pending" : isActive ? " p-2 my-2 rounded-xl  border border-red-900 w-full  " : "p-2 my-2 rounded-xl  border border-red-900 w-full"  } to='/dashboard'>Home</NavLink>
 
                 
                  <NavLink className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "bg-blue-100 p-2 my-2 rounded-xl  border border-red-900 w-full  " : "p-2 my-2 rounded-xl  border border-red-900 w-full"  } to='/dashboard/addProduct'>Add Product</NavLink>
